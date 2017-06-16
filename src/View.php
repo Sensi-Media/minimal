b4a2e945481004fa02789a6097b70582af919d06
Marijn Ophorst <marijn@sensimedia.nl>
2017-06-16 11:56:35 +0200

    our view with a render method

---
+++ src/View.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sensi\Minimal;
+
+use Monolyth\Improse;
+
+class View extends Improse\View
+{
+    public function render()
+    {
+        $html = $this->twig->render($this->template, $this->getVariables());
+        if ($this->env->prod) {
+            $html = preg_replace(['@^\s+@ms', '@\s+(</\w+>)$@m'], ['', '\\1'], $html);
+        }
+        return $html;
+    }
+}
+
