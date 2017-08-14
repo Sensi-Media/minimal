a63dff580c6a6ea9aec19cb2bd88139ee58f8fcc
Marijn Ophorst <marijn@monomelodies.nl>
2017-08-14 15:53:58 +0200

    try/catch and throw frontal exception

--- src/View.php
+++ src/View.php
@@ -4,23 +4,29 @@ namespace Sensi\Minimal;
 
 use Monolyth\Improse;
 use Zend\Diactoros\Response\HtmlResponse;
+use Twig_Loader_Error;
+use Monolyth\Frontal\Exception;
 
 abstract class View extends Improse\View
 {
     public function render()
     {
-        $html = $this->twig->render($this->template, $this->getVariables());
-        if ($this->env->prod) {
-            $html = preg_replace(['@^\s+@ms', '@\s+(</\w+>)$@m'], ['', '\\1'], $html);
+        try {
+            $html = $this->twig->render($this->template, $this->getVariables());
+            if ($this->env->prod) {
+                $html = preg_replace(['@^\s+@ms', '@\s+(</\w+>)$@m'], ['', '\\1'], $html);
+            }
+            $html = preg_replace_callback(
+                '@<title>(.*?)</title>@ms',
+                function ($match) {
+                    return '<title>'.strip_tags($match[1]).'</title>';
+                },
+                $html
+            );
+            return $html;
+        } catch (Twig_Loader_Error $e) {
+            throw new Exception(404);
         }
-        $html = preg_replace_callback(
-            '@<title>(.*?)</title>@ms',
-            function ($match) {
-                return '<title>'.strip_tags($match[1]).'</title>';
-            },
-            $html
-        );
-        return $html;
     }
 
     public function __invoke()
