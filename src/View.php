6e5cf8d30f04d5402f0964c7373708aa42661ac3
Marijn Ophorst <marijn@monomelodies.nl>
2018-01-28 16:43:34 +0100

    documentation, and also keep newlines in excluded patterns

--- src/View.php
+++ src/View.php
@@ -8,35 +8,75 @@ use Zend\Diactoros\Response\HtmlResponse;
 use Twig_Error_Loader;
 use Monolyth\Frontal\Exception;
 
+/**
+ * Custom base view with sensible Sensi defaults:
+ * - strip unnecessary whitespace in production;
+ * - render using Twig;
+ * - throw sensible errors.
+ */
 abstract class View extends Improse\View
 {
     use Injector;
 
-    /** @var int */
+    /**
+     * @var int
+     *
+     * HTTP status code to send. Defaults to 200.
+     */
     protected $status = 200;
 
-    /** @var array */
+    /**
+     * @var array
+     *
+     * Optional hash of extra headers to send. These are in the form
+     * `$key => $value` and will be normalised by Diactoros.
+     */
     protected $headers = [];
 
-    /** @var array */
+    /**
+     * @var array
+     *
+     * Array of regex patterns that should be "excluded" from minimization. In
+     * effect, this means spaces will be replaced by &nbsp;. This is useful e.g.
+     * when showing source code, or content styled with `white-space: pre`.
+     *
+     * Each regex should contain 3 subpatterns: the opening tag, the content and
+     * the closing tag, and must be a fully formed PHP regex string (including
+     * delimiters and optional modifiers).
+     *
+     * <code>
+     * protected $excludePatterns = ['@(<div class="pre">)(.*?)(</div>)@ms'];
+     * </code>
+     */
     protected $excludePatterns = [];
 
-    public function render()
+    /**
+     * Render the template as a string, stripping whitespace on production.
+     *
+     * @return string
+     * @throw Monolyth\Frontal\Exception on any error.
+     * @throw Twig_Error_Loader
+     */
+    public function render() : string
     {
-        $this->inject(function ($env) {});
+        $this->inject(function ($env, $twig) {});
         try {
             $html = $this->twig->render($this->template, $this->getVariables());
-            if ($this->excludePatterns) {
-                $html = preg_replace_callback(
-                    $this->excludePatterns,
-                    function ($match) {
-                        return $match[1].str_replace(' ', '&nbsp;', $match[2]).$match[3];
-                    },
-                    $html
-                );
-            }
             if ($this->env->prod) {
+                if ($this->excludePatterns) {
+                    $html = preg_replace_callback(
+                        $this->excludePatterns,
+                        function ($match) {
+                            $match[2] = str_replace("\n", '___NEW$LINE___', $match[2]);
+                            return $match[1].str_replace(' ', '&nbsp;', $match[2]).$match[3];
+                        },
+                        $html
+                    );
+                }
                 $html = preg_replace(['@^\s+@ms', '@\s+(</\w+>)$@m'], ['', '\\1'], $html);
+                if ($this->excludePatterns) {
+                    $html = str_replace('___NEW$LINE___', "\n", $html);
+                }
             }
             $html = preg_replace_callback(
                 '@<title>(.*?)</title>@ms',
